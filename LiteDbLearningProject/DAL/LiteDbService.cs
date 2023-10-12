5a60d0726e24eb1144f8e2d80cc2a0aa1c0da6b8
Berke Dalar <berkedalar4@gmail.com>
2023-10-12 14:47:01 +0300

    bugs but it will fixed

--- LiteDbLearningProject/DAL/LiteDbService.cs
+++ LiteDbLearningProject/DAL/LiteDbService.cs
@@ -1,6 +1,9 @@
-﻿using LiteDB;
-using ValueHunterDesktop.Models;
+﻿using Dapper;
+using LiteDB;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using ValueHunterDesktop.Models;
 
 namespace LiteDbLearningProject.DAL
 {
@@ -30,10 +33,55 @@ namespace LiteDbLearningProject.DAL
             return Alerts.FindAll();
         }
 
-        
+                public IEnumerable<AlertModel> GetAllAlertsWithDapper()
+        {
+            using (IDbConnection dbConnection = new LiteDbConnection(_db))
+            {
+                dbConnection.Open();
+                return dbConnection.Query<AlertModel>("SELECT * FROM Alerts");
+            }
+        }
+
         public void Dispose()
         {
             _db.Dispose();
         }
+    }
+
+        public class LiteDbConnection : IDbConnection
+    {
+        private readonly LiteDatabase _db;
+
+        public LiteDbConnection(LiteDatabase db)
+        {
+            _db = db;
+        }
+
+        public string ConnectionString { get; set; }
+
+        public int ConnectionTimeout => 30;
+
+        public string Database => "LiteDB";
+
+        public ConnectionState State => ConnectionState.Open;
+
+        public IDbTransaction BeginTransaction() => null;
+
+        public IDbTransaction BeginTransaction(IsolationLevel il) => null;
+
+        public void ChangeDatabase(string databaseName) { }
+
+        public void Close() { }
+
+        public IDbCommand CreateCommand() => new LiteDbCommand();
+
+        public void Open() { }
+
+        public void Dispose() { }
+    }
+
+    public class LiteDbCommand : IDbCommand
+    {
+        
     }
 }
