54dd6f4499e9b52b002c8e8c6e4fd5367baa1057
Berke Dalar <berkedalar4@gmail.com>
2023-10-12 11:10:45 +0300

    updates

--- LiteDbLearningProject/DAL/LiteDbService.cs
+++ LiteDbLearningProject/DAL/LiteDbService.cs
@@ -1,13 +1,35 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValueHunterDesktop.Models;
 
 namespace LiteDbLearningProject.DAL
 {
-    internal class LiteDbService
+    public class LiteDbService
     {
-
+        private readonly LiteDatabase _db;
+        public LiteCollection<AlertModel> Alerts { get; }
+        
+        public LiteDbLearningProject(string database)
+        {
+            _db = new LiteDatabase(database);
+            Alerts = _db.GetCollection<AlertModel>("Alerts");
+        }
+        public void InsertAlert(AlertModel alert)
+        {
+            Alerts.Insert(alert);
+        }
+        public AlertModel FindAlertById(int alertId)
+        {
+            return Alerts.FindById(alertId);
+        }
+        public IEnumerable<AlertModel> GetAllAlerts()
+        {
+            return Alerts.FindAll();
+        }
+    
     }
 }
