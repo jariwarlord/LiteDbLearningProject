d7a9759508717d9790b52756650161658fcb6133
Berke Dalar <berkedalar4@gmail.com>
2023-10-12 11:01:48 +0300

    Initial commit

---
+++ LiteDbLearningProject/Model/AlertModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueHunterDesktop.Models
+{
+    public class AlertModel
+    {
+        public int EstimateID { get; set; }
+        public double TriggerPrice { get; set; }
+        public int AlertID { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LiteDbLearningProject
+ LiteDbProject
